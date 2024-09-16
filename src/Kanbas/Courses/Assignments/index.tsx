44c5ee02044832d9df88d025c64696010481794f
Gray Wu <graywu@Weirans-Air.attlocal.net>
2024-09-16 10:07:33 -0700

    finished 3.7

---
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -0,0 +1,47 @@
+export default function Assignments() {
+  return (
+    <div id="wd-assignments">
+      <input id="wd-search-assignment" placeholder="Search for Assignments" />
+      <button id="wd-add-assignment-group">+ Group</button>
+      <button id="wd-add-assignment">+ Assignment</button>
+      <h3 id="wd-assignments-title">
+        ASSIGNMENTS 40% of Total <button>+</button>
+      </h3>
+      
+      <ul id="wd-assignment-list">
+        {/* Assignment 1 */}
+        <li className="wd-assignment-list-item">
+          <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
+            A1 - ENV + HTML
+          </a>
+          <p>
+            <strong>Multiple Modules</strong> | <strong>Not available until</strong> May 6 at 12:00am | 
+            <strong>Due</strong> May 13 at 11:59pm | 100 pts
+          </p>
+        </li>
+
+        {/* Assignment 2 */}
+        <li className="wd-assignment-list-item">
+          <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
+            A2 - CSS + BOOTSTRAP
+          </a>
+          <p>
+            <strong>Multiple Modules</strong> | <strong>Not available until</strong> May 13 at 12:00am | 
+            <strong>Due</strong> May 20 at 11:59pm | 100 pts
+          </p>
+        </li>
+
+        {/* Assignment 3 */}
+        <li className="wd-assignment-list-item">
+          <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
+            A3 - JAVASCRIPT + REACT
+          </a>
+          <p>
+            <strong>Multiple Modules</strong> | <strong>Not available until</strong> May 20 at 12:00am | 
+            <strong>Due</strong> May 27 at 11:59pm | 100 pts
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,6 +1,7 @@
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
 import { Navigate, Route, Routes } from "react-router";
 
 export default function Courses() {
@@ -18,7 +19,7 @@ export default function Courses() {
               <Route path="/" element={<Navigate to="Home" />} />
               <Route path="Home" element={<Home />} />
               <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments" element={<h2>Assignments</h2>} />
+              <Route path="Assignments" element={<Assignments />} />
               <Route
                 path="Assignments/:aid"
                 element={<h2>Assignment Editor</h2>}
