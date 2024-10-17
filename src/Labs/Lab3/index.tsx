9b2363648702702c842900fcb4a3d35423661b5c
Gray Wu <graywu@Weirans-MacBook-Air.local>
2024-10-17 11:48:13 -0700

    finish 2.3.1 a3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,5 +1,12 @@
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
 
 export default function Lab3() {
     return (
@@ -7,7 +14,14 @@ export default function Lab3() {
         <h3>Lab 3</h3>
         <VariablesAndConstants/>
         <VariableTypes/>
-        
+        <BooleanVariables/>
+        <IfElse/>
+        <TernaryOperator/>
+        <ConditionalOutputIfElse />
+        <ConditionalOutputInline />
+        <LegacyFunctions />
+        <ArrowFunctions />
+
 
       </div>
     );
