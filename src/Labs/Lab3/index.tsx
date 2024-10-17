6a35fb90f7164e9972214be5f48d307b38323cfd
Gray Wu <graywu@Weirans-MacBook-Air.local>
2024-10-17 14:50:56 -0700

    finish 2.4 all a3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -12,6 +12,20 @@ import TemplateLiterals from "./TemplateLiterals";
 import SimpleArrays from "./SimpleArray";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunctions from "./FindFunctions";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+
 
 export default function Lab3() {
     return (
@@ -31,6 +45,19 @@ export default function Lab3() {
         <SimpleArrays />
         <ArrayIndexAndLength />
         <AddingAndRemovingToFromArrays />
+        <ForLoops/>
+        <MapFunction/>
+        <FindFunctions/>
+        <FindIndex/>
+        <FilterFunction/>
+        <JsonStringify/>
+        <House/>
+        <TodoItem/>
+        <TodoList/>
+        <Spreading/>
+        <Destructing/>
+        <FunctionDestructing/>
+        <DestructingImports/>
 
 
 
