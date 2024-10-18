b85b0afcf1e47f493d8bdfeeaae5b923a47ad32e
Gray Wu <graywu@Weirans-MacBook-Air.local>
2024-10-17 19:48:40 -0700

    add pics

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -18,7 +18,9 @@ export default function Dashboard() {
               <div className="card rounded-3 overflow-hidden">
                 <Link className="wd-dashboard-course-link text-decoration-none text-dark"
                       to={`/Kanbas/Courses/${course._id}/Home`}>
-                  <img src="/images/reactjs.jpg" width="100%" height={160} />
+                  {/* <img src="/images/reactjs.jpg" width="100%" height={160} /> */}
+                  <img src={`/images/${course._id}.jpg`} width="100%" height={160} />
+
                   <div className="card-body">
                     <h5 className="wd-dashboard-course-title card-title">
                       {course.name}
