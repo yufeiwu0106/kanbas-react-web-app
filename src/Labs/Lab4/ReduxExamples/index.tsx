b3e1a51706339046b5ffa32378f2cb140125c773
Gray Wu <graywu@Weirans-MacBook-Air.local>
2024-10-31 11:29:56 -0700

    finish 2.4.4

--- src/Labs/Lab4/ReduxExamples/index.tsx
+++ src/Labs/Lab4/ReduxExamples/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HelloRedux from "./HelloRedux";
 import CounterRedux from "./CounterRedux";
+import AddRedux from "./AddRedux";
 
 export default function ReduxExamples() {
   return(
@@ -8,6 +9,7 @@ export default function ReduxExamples() {
       <h2>Redux Examples</h2>
       <HelloRedux/>
       <CounterRedux />
+      <AddRedux />
     </div>
   );
 };
--- src/Labs/store/index.tsx
+++ src/Labs/store/index.tsx
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../Lab4/ReduxExamples/HelloRedux/helloReducer";
 import counterReducer from "../Lab4/ReduxExamples/CounterRedux/counterReducer";
+import addReducer from "../Lab4/ReduxExamples/AddRedux/addReducer";
 const store = configureStore({
-  reducer: { helloReducer, counterReducer },
+  reducer: { helloReducer, counterReducer, addReducer },
 });
 export default store;
