cc2e37acc95304b32509852d34315a18028ff711
Gray Wu <graywu@Weirans-MacBook-Air.local>
2024-10-31 20:13:44 -0700

    finish 3.4.2

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -1,20 +1,40 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { setCurrentUser } from "./reducer";
+import { useDispatch } from "react-redux";
+import * as db from "../Database";
+
 
 export default function Signin() {
+  const [credentials, setCredentials] = useState<any>({});
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const signin = () => {
+    const user = db.users.find(
+      (u: any) => u.username === credentials.username && u.password === credentials.password);
+    if (!user) return;
+    dispatch(setCurrentUser(user));
+    navigate("/Kanbas/Dashboard");
+  };
+
   return (
     <div id="wd-signin-screen">
       <h3>Sign in</h3>
       <input
-        id="wd-username"
-        placeholder="username"
+        defaultValue={credentials.username}
+        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
         className="form-control mb-2"
       />
       <input
-        id="wd-password"
-        placeholder="password"
-        type="password"
+        defaultValue={credentials.password}
+        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
         className="form-control mb-2"
       />
+
+      <button onClick={signin} id="wd-signin-btn" className="btn btn-primary w-100" > Sign in </button>
+      <Link id="wd-signup-link" to="/Kanbas/Account/Signup"> Sign up </Link>
+
+{/* 
       <Link
         id="wd-signin-btn"
         to="/Kanbas/Dashboard"
@@ -25,7 +45,7 @@ export default function Signin() {
       </Link>{" "}
       <Link id="wd-signup-link" to="/Kanbas/Account/Signup">
         Sign up
-      </Link>
+      </Link> */}
     </div>
   );
 }
--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
+import accountReducer from "./Account/reducer";
 
 const store = configureStore({
   reducer: {
     modulesReducer,
+    accountReducer,
   },
 });
 
