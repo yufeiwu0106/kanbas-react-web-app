230f3598ec822b1a0b23feaf99f0647a89d6a19e
Gray Wu <graywu@Weirans-MacBook-Air.local>
2024-10-31 17:33:56 -0700

    finish 3.3.3

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -4,7 +4,6 @@ import ModuleControlButtons from "./ModuleControlButtons";
 import { BsGripVertical } from "react-icons/bs";
 import React, { useState } from "react";
 
-
 import { useParams } from "react-router";
 import * as db from "../../Database";
 
@@ -13,19 +12,36 @@ export default function Modules() {
   const [modules, setModules] = useState<any[]>(db.modules);
   const [moduleName, setModuleName] = useState("");
   const addModule = () => {
-    setModules([ ...modules, { _id: new Date().getTime().toString(),
-                                     name: moduleName, course: cid, lessons: [] } ]);
+    setModules([
+      ...modules,
+      {
+        _id: new Date().getTime().toString(),
+        name: moduleName,
+        course: cid,
+        lessons: [],
+      },
+    ]);
     setModuleName("");
   };
   const deleteModule = (moduleId: string) => {
     setModules(modules.filter((m) => m._id !== moduleId));
   };
-
-
+  const editModule = (moduleId: string) => {
+    setModules(
+      modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m))
+    );
+  };
+  const updateModule = (module: any) => {
+    setModules(modules.map((m) => (m._id === module._id ? module : m)));
+  };
 
   return (
     <div className="container">
-      <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule}/>
+      <ModulesControls
+        setModuleName={setModuleName}
+        moduleName={moduleName}
+        addModule={addModule}
+      />
       <br />
       <br />
       <br />
@@ -36,9 +52,29 @@ export default function Modules() {
           .map((module: any) => (
             <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
               <div className="wd-title p-3 ps-2 bg-secondary">
+
+                
                 <BsGripVertical className="me-2 fs-3" />
-                {module.name}
-                <ModuleControlButtons moduleId={module._id} deleteModule={deleteModule}/>
+                {!module.editing && module.name}
+                {module.editing && (
+                  <input className="form-control w-50 d-inline-block"
+                    onChange={(e) =>
+                      updateModule({ ...module, name: e.target.value })}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        updateModule({ ...module, editing: false }); }
+                    }}
+                    defaultValue={module.name}/>
+                )}
+                
+
+                <ModuleControlButtons
+                  moduleId={module._id}
+                  deleteModule={deleteModule}
+                  editModule={editModule}/>
+
+
+
               </div>
               {module.lessons && (
                 <ul className="wd-lessons list-group rounded-0">
