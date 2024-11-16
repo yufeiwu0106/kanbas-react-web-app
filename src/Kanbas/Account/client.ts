84b85bf9f378091cc6fb0ce69ae4d66e81dc0268
Gray Wu <graywu@Weirans-MacBook-Air.local>
2024-11-16 13:56:35 -0800

    finish 4.3

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -2,28 +2,30 @@ import axios from "axios";
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
+const axiosWithCredentials = axios.create({ withCredentials: true });
+
 export const signin = async (credentials: any) => {
-    const response = await axios.post(`${USERS_API}/signin`, credentials);
+    const response = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
     return response.data;
 };
 
 export const signup = async (user: any) => {
-    const response = await axios.post(`${USERS_API}/signup`, user);
+    const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
     return response.data;
 };
 
 export const signout = async () => {
-    const response = await axios.post(`${USERS_API}/signout`);
+    const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
     return response.data;
 };
 
 export const updateUser = async (user: any) => {
-    const response = await axios.put(`${USERS_API}/${user._id}`, user);
+    const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
     return response.data;
 };
 
 export const profile = async () => {
-    const response = await axios.post(`${USERS_API}/profile`);
+    const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
     return response.data;
 };
 
